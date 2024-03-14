c2abe7777992db3d74cc3c8f82353e5c314dde48
VkTheory <86123436+VicTheory@users.noreply.github.com>
2024-03-13 20:17:16 -0700

    Encriptador Alura

---
+++ README.md
@@ -0,0 +1,19 @@
+# Encriptador Alura
+
+El Encriptador Alura es un proyecto de desarrollo academico.
+
+## Caracteristicas
+
+El Encriptador Alura encripta unicamente textos, con un esquema simple de remplazo de caracteres en cadena. Fue contruido siguiendo los conceptos basicos de la programacion en HTML, CSS y lenguaje Javascript.
+
+## Code
+```javascript
+
+// La letra "e" es convertida para "enter"
+// La letra "i" es convertida para "imes"
+// La letra "a" es convertida para "ai"
+// La letra "o" es convertida para "ober"
+// La letra "u" es convertida para "ufat"
+
+let matrizCodigo = [["e","enter"],["i","imes"],["a","ai"],["o","ober"],["u","ufat"],[" ","_"]];
+```
---
+++ app.js
@@ -0,0 +1,58 @@
+const textArea = document.querySelector(".text-wrapper-2");
+const mensaje = document.querySelector(".mesage");
+const actCopiar = document.querySelector(".button-wrapper2");
+
+function actEncriptar(){
+    const textoEncriptado = encriptar(textArea.value).trim();
+    mensaje.value = textoEncriptado;
+    textArea.value = "";
+    document.querySelector(".mueco").style.display = "none";
+    document.querySelector(".text-wrapper").style.visibility = "hidden";
+    document.querySelector(".button-wrapper2").style.visibility = "visible";
+    document.querySelector(".frame").classList = "frame::after";
+    mensaje.classList ="mesage2";
+    //console.log(mensaje.value);
+}
+
+// La letra "e" es convertida para "enter"
+// La letra "i" es convertida para "imes"
+// La letra "a" es convertida para "ai"
+// La letra "o" es convertida para "ober"
+// La letra "u" es convertida para "ufat"
+
+function encriptar(stringEncriptada){
+    let matrizCodigo = [["e","enter"],["i","imes"],["a","ai"],["o","ober"],["u","ufat"],[" ","_"]];
+    stringEncriptada = stringEncriptada.toLowerCase();
+
+    for(let i = 0; i < matrizCodigo.length; i++){
+        if(stringEncriptada.includes(matrizCodigo[i][0])){
+            stringEncriptada = stringEncriptada.replaceAll(matrizCodigo[i][0], matrizCodigo[i][1]);
+        }
+    }
+    return stringEncriptada;
+}
+function actDesencriptar(){
+    const textoEncriptado = desencriptar(textArea.value).trim();
+    mensaje.value = textoEncriptado;
+    textArea.value = "";
+}
+
+function desencriptar(stringDesencriptada){
+    let matrizCodigo = [["e","enter"],["i","imes"],["a","ai"],["o","ober"],["u","ufat"],[" ","_"]];
+    stringDesencriptada = stringDesencriptada.toLowerCase();
+
+    for(let i = 0; i < matrizCodigo.length; i++){
+        if(stringDesencriptada.includes(matrizCodigo[i][1])){
+            stringDesencriptada = stringDesencriptada.replaceAll(matrizCodigo[i][1], matrizCodigo[i][0]);
+        }
+    }
+    return stringDesencriptada;
+}
+
+actCopiar.addEventListener("click", copyText);
+function copyText(mensaje){
+    mensaje.select();
+    document.execCommand("copy");
+    window.getSelection().removeAllRanges();
+    alert("Copiado");
+};
